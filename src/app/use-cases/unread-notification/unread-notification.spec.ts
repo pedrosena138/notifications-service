c505edd2b3716785ae193a5059826560d552f064
Pedro Sena <pmfsl@cin.ufpe.br>
2022-12-15 15:41:23 -0300

    lesson 03 - unread notification use case

--- src/app/use-cases/unread-notification/unread-notification.spec.ts
+++ src/app/use-cases/unread-notification/unread-notification.spec.ts
@@ -2,32 +2,30 @@ import { randomUUID } from 'node:crypto';
 import { InMemoryNotificationRepository } from '@test/repositories/in-memory-notification.repository';
 import { NotificationNotFound } from '@app/errors/notification-not-found';
 import { makeNotification } from '@test/factories/notification-factory';
-import { ReadNotification } from './read-notification';
+import { UnreadNotification } from './unread-notification';
 
-describe('Read notification use case', () => {
+describe('Unread notification use case', () => {
   const makeSUT = () => {
     const notificationRepository = new InMemoryNotificationRepository();
-    const sut = new ReadNotification(notificationRepository);
+    const sut = new UnreadNotification(notificationRepository);
 
     return { sut, notificationRepository };
   };
 
-  it('should be able to read a notification', async () => {
+  it('should be able to unread a notification', async () => {
     const { sut, notificationRepository } = makeSUT();
 
-    const notification = makeNotification();
+    const notification = makeNotification({ readAt: new Date() });
 
     await notificationRepository.create(notification);
     await sut.execute({
       notificationId: notification.id,
     });
 
-    expect(notificationRepository.notifications[0].readAt).toEqual(
-      expect.any(Date),
-    );
+    expect(notificationRepository.notifications[0].readAt).toBeNull();
   });
 
-  it('should not be able to read a notification that not exists', async () => {
+  it('should not be able to unread a notification that not exists', async () => {
     const { sut } = makeSUT();
 
     const result = () => {
